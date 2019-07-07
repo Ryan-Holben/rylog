eab94675b8620cf9c5545532a40710116019eb69
Ryan Holben <rgholben@gmail.com>
2019-07-06 20:08:50 -0700

    Added setup in order to create a package for distribution via pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rylog',
+    version='0.0.1',
+    description='simple yet functional logging for Python 3.x',
+    license='MIT',
+    packages=['rylog'],
+    author='Ryan Holben',
+    author_email='rgholben@gmail.com',
+    keywords=['python', 'python3', 'logging'],
+    url='https://github.com/Ryan-Holben/rylog')
